e7a251b998d2d725ad2d2f40f74d286f5e6b80e3
zuhaib-a <zuhaib.a@live.co.uk>
2023-02-24 15:59:55 +0000

    first attempt at config

--- my_first_app/app.py
+++ my_first_app/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+
+app.config.from_object(Config)
 
 if __name__ == '__main__':
     app.run(debug=True)
