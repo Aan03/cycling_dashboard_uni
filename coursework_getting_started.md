5f8bad018acc6ba4a8e192b8a607fc94faac53d7
zuhaib-a <zuhaib.a@live.co.uk>
2023-03-10 14:32:59 +0000

    getting index to work

--- coursework_getting_started.md
+++ coursework_getting_started.md
@@ -126,8 +126,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    return render_template("templates.index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main_flask_app/app.py
+++ main_flask_app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-#app.config.from_object(Config)
 
 @app.route('/')
 def hello_world():
-   # return 'Hello World!'
-    return render_template("templates.index")
-    #return app.config['SECRET_KEY']
+    return render_template("templates.index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
