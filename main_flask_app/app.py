1fc0e095e94e4c9caa133aa5207e6ff438f5b62c
zuhaib-a <zuhaib.a@live.co.uk>
2023-03-10 16:02:34 +0000

    nav works

--- main_flask_app/app.py
+++ main_flask_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from example_blueprint.main_blueprint import example_blueprint
+
 
 app = Flask(__name__)
+app.register_blueprint(example_blueprint)
 
-@app.route('/')
-def hello_world():
-    return render_template("templates.index.html")
+#@app.route('/')
+#def hello_world():
+#    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- my_first_app/app.py
+++ my_first_app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates')
 #app.config.from_object(Config)
 
 @app.route('/')
 def hello_world():
    # return 'Hello World!'
-    return render_template("templates.index")
+    return render_template("templates.index.html")
     #return app.config['SECRET_KEY']
 
 if __name__ == '__main__':
