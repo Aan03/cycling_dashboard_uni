d534853263e21d63e36f40d2d5836f1e78f616df
zuhaib-a <zuhaib.a@live.co.uk>
2023-03-10 18:48:27 +0000

    dash app added and works

--- main_flask_app/app.py
+++ main_flask_app/app.py
@@ -1,19 +1,12 @@
 from flask import Flask, render_template
 from multi_page_app.stats import create_dash_app
 from multi_page_app import *
-from example_blueprint.main_blueprint import example_blueprint
-
+from main_bp.main_bp import main_bp
 
 app = Flask(__name__)
-app.register_blueprint(example_blueprint)
-
+app.register_blueprint(main_bp)
 
 create_dash_app(app)
-
-
-#@app.route('/')
-#def hello_world():
-#    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
