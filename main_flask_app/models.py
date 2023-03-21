798c829133fa50be55b0c4f26011c9d518e61729
zuhaib-a <zuhaib.a@live.co.uk>
2023-03-21 04:41:18 +0000

    filters added, nav & report format fixed, cleaned

--- main_flask_app/models.py
+++ main_flask_app/models.py
@@ -11,7 +11,6 @@ class user(db.Model, UserMixin):
     #email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
-<<<<<<< HEAD
 class reports(db.Model, UserMixin):
     #__tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
@@ -20,8 +19,6 @@ class reports(db.Model, UserMixin):
     report_time = db.Column(db.String(1000))
     #email = db.Column(db.String(100), unique=True)
     
-=======
->>>>>>> 4550cb4e1a8097c667544f2db72255c03c7e74c7
 
 class favourites(UserMixin, db.Model):
 
@@ -29,11 +26,3 @@ class favourites(UserMixin, db.Model):
     username = db.Column(db.String(1000))
     #email = db.Column(db.String(100), unique=True)
     favourite = db.Column(db.String(100))
-
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> 4550cb4e1a8097c667544f2db72255c03c7e74c7
