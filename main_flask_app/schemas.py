5572af226a2e1614fc3594ebcec1808d1882ac61
Aan03 <zcemail@ucl.ac.uk>
2023-03-24 14:37:56 +0000

    Failed attempt at download filterted csv file

--- main_flask_app/schemas.py
+++ main_flask_app/schemas.py
@@ -1,7 +1,18 @@
-from main_flask_app import ma
-'''
-class ReportsSchema(ma.SQLALchemySchema):
-""" Marshmallow schema defining the attributes for creating reports."""
+from main_flask_app import db, ma
+from main_flask_app.models import Users, Reports
 
-class Meta:
-model = Reports'''
+
+class ReportsSchema(ma.SQLAlchemySchema):
+    """Marshmallow schema defining the attributes for creating a report."""
+
+    class Meta:
+        model = Reports
+        load_instance = True
+        sqla_session = db.session
+        include_relationships = True
+
+    rack_id = ma.auto_field()
+    report_borough = ma.auto_field()
+    report_date = ma.auto_field()
+    report_time = ma.auto_field()
+    report_details = ma.auto_field()
