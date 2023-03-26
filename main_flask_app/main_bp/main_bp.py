e270553417b2a975592c004dad08cb96feca7952
zuhaib-a <zuhaib.a@live.co.uk>
2023-03-26 16:19:36 +0100

    dataset loaded from sql now, other clean up

--- main_flask_app/main_bp/main_bp.py
+++ main_flask_app/main_bp/main_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session, flash, jsonify
 from flask_login import UserMixin, login_required, current_user
-from sqlalchemy import desc
+#from sqlalchemy import desc
 from main_flask_app.dash_app_cycling import *
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, TimeField, validators
@@ -8,13 +8,47 @@ import csv
 import os
 import json
 import pandas as pd
-from main_flask_app import db
-from main_flask_app.models import Users, Reports
+from main_flask_app import db, db_session
+from main_flask_app.models import Users, Reports, cycle_parking_data, boroughs_list
 
 main_bp = Blueprint('main_bp', __name__, template_folder = "templates", static_folder="static")
 basedir = os.path.abspath(os.path.dirname(__file__))
 maindir = os.path.abspath(os.path.join(basedir, os.pardir))
 
+new_boroughs = []
+for x in db_session.query(boroughs_list.borough):
+    new_boroughs.append(list(x))
+new_boroughs = [element for nestedlist in new_boroughs for element in nestedlist]
+print(new_boroughs)
+
+marker_data = []
+for x in db_session.query(cycle_parking_data.feature_id, cycle_parking_data.prk_cover,
+                          cycle_parking_data.prk_secure, cycle_parking_data.prk_locker,
+                          cycle_parking_data.prk_cpt, cycle_parking_data.borough,
+                          cycle_parking_data.photo1_url, cycle_parking_data.photo2_url,
+                          cycle_parking_data.latitude, cycle_parking_data.longitude):
+    rack_type_converted = []
+    for i in range(1,4):
+        if x[i] == "1":
+            rack_type_converted.append("True")
+        if x[i] == "0":
+            rack_type_converted.append("False")
+
+        
+    each_row = {"feature_id":x[0],
+                "prk_cover":rack_type_converted[0],
+                "prk_secure":rack_type_converted[1],
+                "prk_locker":rack_type_converted[2],
+                "prk_cpt":x[4],
+                "borough":x[5],
+                "photo1_url":x[6],
+                "photo2_url":x[7],
+                "latitude":x[8],
+                "longitude":x[9]
+                }
+    marker_data.append(each_row)
+
+print(marker_data[1])
 @main_bp.route('/my_reports', methods=['POST', 'GET'])
 @login_required
 def my_reports():
@@ -25,10 +59,9 @@ def my_reports():
                                user_reports_count = user_reports_count)
     
     elif request.method == "POST":
-        flash("Tested")
+        flash("Your reports have been updated.")
         return render_template('my_reports.html', reports_table = reports_table)
 
-    
 @main_bp.route("/edit_report_details/<int:report_id>", methods=['POST'])
 @login_required
 def edit_report_details(report_id):
@@ -65,7 +98,9 @@ def dash():
 @main_bp.route("/reports")
 def reports_page():
     reports_table = Reports.query.order_by(Reports.report_date.desc(), Reports.report_time.desc())
-    return render_template("reports.html", reports_table=reports_table)
+    user_reports_count = Reports.query.count()
+    return render_template("reports.html", reports_table=reports_table, 
+                           user_reports_count = user_reports_count)
 
 myFile = open(maindir + "/data/cycle_parking_data.csv", "r")
 
@@ -74,26 +109,6 @@ myList = list()
 for dictionary in reader:
     myList.append(dictionary)
 markers_info = (myList)
-
-rack_id_list = pd.read_csv(maindir + "/data/cycle_parking_data.csv", 
-                           usecols = [0])
-
-rack_id_list = rack_id_list.values.tolist()
-rack_id_list = [elem for sublist in rack_id_list for elem in sublist]
-
-myFile = open(maindir + "/data/cycle_parking_coordinates.csv", "r")
-reader = csv.DictReader(myFile)
-myList = list()
-for dictionary in reader:
-    myList.append(dictionary)    
-markers = (myList)
-
-myFile = open(maindir + "/data/boroughs_list.csv", "r")
-reader = csv.reader(myFile)
-myList = list()
-for borough in reader:
-    myList = myList + borough    
-boroughs = (myList)
 
 class ReportForm(FlaskForm):
     report_rack_id = StringField("Rack ID", validators = [validators.Length(min=9, max=9), validators.DataRequired()])
@@ -107,9 +122,9 @@ class ReportForm(FlaskForm):
 def index():
     report_form = ReportForm()
     if request.method == "GET":
-        return render_template('index.html', markers=json.dumps(markers), 
-                           markers_info=json.dumps(markers_info), rack_id_list=rack_id_list,
-                           boroughs = boroughs, report_form = report_form)
+        return render_template('index.html', 
+                           markers_info=json.dumps(marker_data), boroughs = new_boroughs,
+                           report_form = report_form)
     
     elif request.method == "POST":
         reporter_id = current_user.id
